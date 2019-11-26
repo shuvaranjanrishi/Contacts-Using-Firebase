10e6fc1a66583531df2e575c384d7cc9d16f450a
shuvaranjanrishi <shopnershopnil44@gmail.com>
2019-11-26 11:03:23 +0600

    sign up activity created

--- app/src/main/java/com/example/contactsusingfirebase/SignUpActivity.java
+++ app/src/main/java/com/example/contactsusingfirebase/SignUpActivity.java
@@ -2,19 +2,17 @@ package com.example.contactsusingfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
     }
 
-    public void SignUpTVClickAction(View view) {
-        startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+    public void SignUpBtnClickAction(View view) {
     }
 }
