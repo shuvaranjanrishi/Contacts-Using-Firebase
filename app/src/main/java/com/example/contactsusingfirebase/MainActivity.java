a0d1f2adccee5fdc83f4f1c0be2e51155ae6207f
shuvaranjanrishi <shopnershopnil44@gmail.com>
2019-11-26 16:50:20 +0600

    initialization complete in main activity

--- app/src/main/java/com/example/contactsusingfirebase/MainActivity.java
+++ app/src/main/java/com/example/contactsusingfirebase/MainActivity.java
@@ -1,17 +1,49 @@
 package com.example.contactsusingfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<Contact> contactList;
+    private ContactAdapter contactAdapter;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+
+       // getDataFromFirebase();
+    }
+
+//    private void getDataFromFirebase() {
+//        String userId = firebaseAuth.getCurrentUser().getUid();
+//        databaseReference.child("users").child(userId).child("contacts").getKey();
+//    }
+
+    private void initialize() {
+        recyclerView = findViewById(R.id.recyclerViewId);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        contactList = new ArrayList<>();
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 
     public void fabClickAction(View view) {
