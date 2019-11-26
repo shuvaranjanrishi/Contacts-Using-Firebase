ce9a2e9db280d47dd3045444d118b1c0fa8930eb
Shonpnil <shopnershopnil44@gmail.com>
2019-11-26 23:02:43 +0600

    contact read successfully

--- app/src/main/java/com/example/contactsusingfirebase/MainActivity.java
+++ app/src/main/java/com/example/contactsusingfirebase/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.contactsusingfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,13 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
         initialize();
 
-       // getDataFromFirebase();
+        getDataFromFirebase();
     }
 
-//    private void getDataFromFirebase() {
-//        String userId = firebaseAuth.getCurrentUser().getUid();
-//        databaseReference.child("users").child(userId).child("contacts").getKey();
-//    }
+    private void getDataFromFirebase() {
+        String userId = firebaseAuth.getCurrentUser().getUid();
+        DatabaseReference contactsRef = databaseReference.child("users").child(userId).child("contacts");
+
+        contactsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+
+                    contactList.clear();
+
+                    for (DataSnapshot data : dataSnapshot.getChildren()) {
+
+                        Contact contact = data.getValue(Contact.class);
+                        contactList.add(contact);
+                        contactAdapter = new ContactAdapter(contactList, MainActivity.this);
+                        recyclerView.setAdapter(contactAdapter);
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "No Contact Found !", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("Read Data Failed: " + databaseError.getMessage());
+            }
+        });
+    }
 
     private void initialize() {
         recyclerView = findViewById(R.id.recyclerViewId);
@@ -47,6 +79,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void fabClickAction(View view) {
-        startActivity(new Intent(MainActivity.this,AddContactActivity.class));
+        startActivity(new Intent(MainActivity.this, AddContactActivity.class));
     }
 }
