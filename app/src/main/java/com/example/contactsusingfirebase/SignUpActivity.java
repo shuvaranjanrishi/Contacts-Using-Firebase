4d22c68752fa3e69790034aec3796d7c36635a1e
shuvaranjanrishi <shopnershopnil44@gmail.com>
2019-11-26 11:27:05 +0600

    sign up inputs validation complete

--- app/src/main/java/com/example/contactsusingfirebase/SignUpActivity.java
+++ app/src/main/java/com/example/contactsusingfirebase/SignUpActivity.java
@@ -4,15 +4,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText nameET,emailET,passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        initialize();
     }
 
     public void SignUpBtnClickAction(View view) {
+
+        String name = nameET.getText().toString().trim();
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        if(!validate(name,email,password)){
+            return;
+        }else {
+
+        }
     }
+
+    private boolean validate(String name, String email, String password) {
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(name.isEmpty()){
+            nameET.setError("Please Enter Your Name");
+            return false;
+        }else if(name.length() < 3){
+            nameET.setError("Name should be at least 3 character");
+            return false;
+        }else if(email.isEmpty()){
+            emailET.setError("Please Enter Your Email");
+            return false;
+        }else if(!email.matches(emailPattern)){
+            emailET.setError("Please Enter a Valid Email");
+            return false;
+        }else if(password.isEmpty()){
+            passwordET.setError("Please Enter a Password");
+            return false;
+        }else if(password.length() < 6){
+            passwordET.setError("Password should be at least 6 character");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void initialize() {
+        nameET = findViewById(R.id.nameETId);
+        emailET = findViewById(R.id.emailETId);
+        passwordET = findViewById(R.id.passwordETId);
+    }
+
 }
