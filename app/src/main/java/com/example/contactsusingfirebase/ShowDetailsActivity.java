174873cc55e113e6a27b7d63ed5f3dcef82c4296
shuvaranjanrishi <shopnershopnil44@gmail.com>
2019-11-27 11:14:10 +0600

    show details activity designed

--- app/src/main/java/com/example/contactsusingfirebase/ShowDetailsActivity.java
+++ app/src/main/java/com/example/contactsusingfirebase/ShowDetailsActivity.java
@@ -1,4 +1,22 @@
 package com.example.contactsusingfirebase;
 
-public class ShowDetailsActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowDetailsActivity  extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_details);
+
+
+    }
+
+    public void deleteBtnClickAction(View view){
+        Toast.makeText(this, "delete", Toast.LENGTH_SHORT).show();
+    }
 }
