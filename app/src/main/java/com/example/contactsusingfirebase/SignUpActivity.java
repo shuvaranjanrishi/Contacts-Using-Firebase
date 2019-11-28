1caa1f02e44fae8a843959acb2b1833f01eb98cf
Shonpnil <shopnershopnil44@gmail.com>
2019-11-28 22:29:14 +0600

    back button added in signUp page

--- app/src/main/java/com/example/contactsusingfirebase/SignUpActivity.java
+++ app/src/main/java/com/example/contactsusingfirebase/SignUpActivity.java
@@ -30,6 +30,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         initialize();
     }
